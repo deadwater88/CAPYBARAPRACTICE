b02bc8737ff308f317fc594383af3e2868341150
App Academy Student <student@appacademy.io>
2017-04-21 11:44:05 -0700

    write specs for user model and implement user model

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  attr_reader :password
+  validates :username, :password_digest, :session_token, :password, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(32)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(32)
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+require 'shoulda-matchers'
+
+RSpec.describe User, type: :model do
+  subject!(:user){ User.create(username: 'david', password: 'password' )}
+
+  describe "validation" do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:session_token) }
+  end
+
+  describe "associations" do
+    it { should have_many(:goals) }
+    it { should have_many(:comments) }
+  end
+
+  describe "::find_by_credentials" do
+    it 'should a return a user given valid credentials' do
+      expect(User.find_by_credentials('david', 'password').attributes).to eq(user.attributes)
+    end
+
+    it 'should return nil if given invalid credentials' do
+      expect(User.find_by_credentials('sam', 'password')).to be_nil
+      expect(User.find_by_credentials('david', 'shark')).to be_nil
+    end
+
+  end
+
+  describe '#reset_session_token!' do
+    it 'changes session token' do
+      pre_token = user.session_token
+      user.reset_session_token!
+      post_token = user.session_token
+      expect(pre_token).to_not eq(post_token)
+    end
+  end
+
+  describe '#password=' do
+    it 'stores password as salted hash' do
+      expect(BCrypt::Password.new(user.password_digest).is_password?(user.password)).to be true
+    end
+    it 'passwords are not stored' do
+      expect(User.first.password).to be_nil
+    end
+  end
+
+  describe '#is_password?' do
+    it 'returns true if passwords match' do
+      expect(user.is_password?('password')).to be true
+    end
+    it 'returns false otherwise' do
+      expect(user.is_password?('shark')).to be false
+    end
+  end
+
+
+
+
+end
